74600bf3fdeafbc54f6c5b84acaa1bfa1906c90b
Mahdi Alizadeh <mahdialz.dev@gmail.com>
2026-08-17 18:21:51 +0330

    extract NewFactForm component

---
+++ src/components/NewFactForm.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import supabase from "../supabase";
+import { CATEGORIES } from "../constants";
+import { isValidHttpUrl } from "../utils";
+
+export function NewFactForm({ setFacts, setShowForm }) {
+  const [text, setText] = useState("");
+  const [source, setSource] = useState("");
+  const [category, setCategory] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (text && isValidHttpUrl(source) && category && text.length <= 200) {
+      setIsUploading(true);
+      const { data: newFact, error } = await supabase
+        .from("facts")
+        .insert([{ text, source, category }])
+        .select();
+      setIsUploading(false);
+      if (!error) setFacts((facts) => [newFact[0], ...facts]);
+      setText("");
+      setSource("");
+      setCategory("");
+      setShowForm(false);
+    }
+  }
+
+  return (
+    <form className="fact-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Share a fact with the world..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        disabled={isUploading}
+      />
+      <span>{200 - text.length}</span>
+      <input
+        type="text"
+        placeholder="Trustworthy source..."
+        value={source}
+        onChange={(e) => setSource(e.target.value)}
+        disabled={isUploading}
+      />
+      <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+        disabled={isUploading}
+      >
+        <option value="">Choose category:</option>
+        {CATEGORIES.map((cat) => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      <button className="btn btn-large" disabled={isUploading}>
+        Post
+      </button>
+    </form>
+  );
+}
