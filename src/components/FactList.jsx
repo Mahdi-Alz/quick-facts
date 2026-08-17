a808674d329b56be8105f1dba5870ad0e3cb6ab8
Mahdi Alizadeh <mahdialz.dev@gmail.com>
2026-08-17 18:22:04 +0330

    extract FactList component

---
+++ src/components/FactList.jsx
@@ -0,0 +1,21 @@
+import { Fact } from "./Fact";
+
+export function FactList({ facts, setFacts }) {
+  if (facts.length === 0) {
+    return (
+      <p className="message">
+        no facts for this category yet! create the first one
+      </p>
+    );
+  }
+
+  return (
+    <section>
+      <ul className="facts-list">
+        {facts.map((fact) => (
+          <Fact key={fact.id} fact={fact} setFacts={setFacts} />
+        ))}
+      </ul>
+    </section>
+  );
+}
